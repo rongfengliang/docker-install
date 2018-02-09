2a1940640b6e208ea0814bca0d95abc9042c9243
liulei3 <871607680@qq.com>
2018-02-09 15:37:21 +0800

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -3,3 +3,4 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://099430a4.m.daocloud.io
+systemctl restart docker
